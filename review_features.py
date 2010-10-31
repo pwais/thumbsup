875eeff9b6de2f42668e73d4c77ad09c537b7225
Paul Wais <pwais@uscribbles.(none)>
2010-10-31 16:52:22 -0700

    Use word frequencies instead of counts

--- review_features.py
+++ review_features.py
@@ -2,24 +2,29 @@ import re
 
 import sat_gre_words
 
-def __fill_special_word_count(review, key, word_set):
-	"""Fill `key` in `review` with a count of the number
+def __fill_special_word_freq(review, key, word_set):
+	"""Fill `key` in `review` with the frequency of the 
 	of review words that appear in `word_set`."""
 
 	iter_matches = re.finditer("(\w+)", review['text'])
 	iter_words = (m.groups()[0] for m in iter_matches)
-	review[key] = sum(1 for word in iter_words
-						if word.lower() in word_set)
+	word_count = 0
+	total_num_words = 0
+	for word in iter_words:
+		total_num_words += 1
+		if word.lower() in word_set:
+			word_count += 1
+	review[key] = float(word_count) / total_num_words
 
-def fill_gre_word_count(review):
-	__fill_special_word_count(review, 'gre_word_count', sat_gre_words.GRE_WORDS)
+def fill_gre_word_freq(review):
+	__fill_special_word_freq(review, 'gre_word_freq', sat_gre_words.GRE_WORDS)
 
-def fill_sat_word_count(review):
-	__fill_special_word_count(review, 'sat_word_count', sat_gre_words.SAT_WORDS)
+def fill_sat_word_freq(review):
+	__fill_special_word_freq(review, 'sat_word_freq', sat_gre_words.SAT_WORDS)
 
 
 def fill_review_features(review):
 	"""Fill all review features in `review`"""
-	fill_gre_word_count(review)
-	fill_sat_word_count(review)
+	fill_gre_word_freq(review)
+	fill_sat_word_freq(review)
 
