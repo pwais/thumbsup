e8f7ca2428a1404fdacd2bbde2c48f01a23cda48
Paul Wais <pwais@quad.local>
2010-10-31 23:37:55 -0700

    tabs -> spaces

--- review_features.py
+++ review_features.py
@@ -3,35 +3,35 @@ from spellchecker import is_typo
 import sat_gre_words
 
 def __fill_special_word_freq(review, key, word_set):
-	"""Fill `key` in `review` with the frequency of the 
-	of review words that appear in `word_set`."""
+    """Fill `key` in `review` with the frequency of the 
+    of review words that appear in `word_set`."""
 
-	iter_matches = re.finditer("(\w+)", review['text'])
-	iter_words = (m.groups()[0] for m in iter_matches)
-	word_count = 0
-	total_num_words = 0
-	for word in iter_words:
-		total_num_words += 1
-		if word.lower() in word_set:
-			word_count += 1
-	review[key] = float(word_count) / total_num_words
+    iter_matches = re.finditer("(\w+)", review['text'])
+    iter_words = (m.groups()[0] for m in iter_matches)
+    word_count = 0
+    total_num_words = 0
+    for word in iter_words:
+        total_num_words += 1
+        if word.lower() in word_set:
+            word_count += 1
+    review[key] = float(word_count) / total_num_words
 
 def fill_gre_word_freq(review):
-	__fill_special_word_freq(review, 'gre_word_freq', sat_gre_words.GRE_WORDS)
+    __fill_special_word_freq(review, 'gre_word_freq', sat_gre_words.GRE_WORDS)
 
 def fill_sat_word_freq(review):
-	__fill_special_word_freq(review, 'sat_word_freq', sat_gre_words.SAT_WORDS)
+    __fill_special_word_freq(review, 'sat_word_freq', sat_gre_words.SAT_WORDS)
 
 def fill_review_typos(review):
-	'''Fill compute the number of typos in the text of the review
-	and add it to review["typos"]'''
-	num_typos = 0
-	words = [word.lower() for word in re.findall('\w+', review['text'])]
-	for word in words:
-		if is_typo(word):
-			num_typos += 1
-	review['typos'] = num_typos
-		
+    '''Fill compute the number of typos in the text of the review
+    and add it to review["typos"]'''
+    num_typos = 0
+    words = [word.lower() for word in re.findall('\w+', review['text'])]
+    for word in words:
+        if is_typo(word):
+            num_typos += 1
+    review['typos'] = num_typos
+        
 def __fill_word_count(review, key):
     """The number of words in the review"""
     words=re.split('\w+',review['text'])
@@ -53,9 +53,9 @@ def fill_ave_words_per_sentence(review):
 
 # Fill everything
 def fill_all_review_features(review):
-	"""Fill all review features in `review`"""
-	fill_gre_word_freq(review)
-	fill_sat_word_freq(review)
-	fill_word_count(review)
-	fill_ave_words_per_sentence(review)
-	fill_review_typos(review)
+    """Fill all review features in `review`"""
+    fill_gre_word_freq(review)
+    fill_sat_word_freq(review)
+    fill_word_count(review)
+    fill_ave_words_per_sentence(review)
+    fill_review_typos(review)
--- reviews_to_features.py
+++ reviews_to_features.py
@@ -7,22 +7,21 @@ import simplejson
 from review_features import fill_review_features
 
 if __name__ == '__main__':
-	
-	review_keys = None
-	feature_keys = None
+    
+    review_keys = None
+    feature_keys = None
 
-	for review in csv.DictReader(sys.stdin):
-		if review_keys is None:
-			review_keys = set(review.keys())
+    for review in csv.DictReader(sys.stdin):
+        if review_keys is None:
+            review_keys = set(review.keys())
 
-		fill_all_review_features(review)
+        fill_all_review_features(review)
 
-		if feature_keys is None:
-			feature_keys = set(review.keys()) - review_keys
+        if feature_keys is None:
+            feature_keys = set(review.keys()) - review_keys
 
-		for key in review_keys:
-			if key != 'id':
-				del review[key]
-		
-		print >>sys.stdout, simplejson.dumps(review)
-
+        for key in review_keys:
+            if key != 'id':
+                del review[key]
+        
+        print >>sys.stdout, simplejson.dumps(review)
