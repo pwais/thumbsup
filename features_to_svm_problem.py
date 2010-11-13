8f09596d4d9320ab1e6c89a431db2a1cdae58e47
Paul Wais <pwais@uscribbles.(none)>
2010-11-13 15:32:48 -0800

    refactor features to use a feature_ key prefix

--- features_to_svm_problem.py
+++ features_to_svm_problem.py
@@ -10,25 +10,19 @@ import sys
 import simplejson
 
 LABEL_FEATURE = 'amazon_frac_voted_useful'
-INPUT_FEATURES = ('gre_word_freq',
-                  'sat_word_freq',
-                  'ave_words_per_sent',
-                  'word_count',
-                  #'typos',  skip this feature for now, it's slow
-				  'valence_score',
-				  'num_urls',
-				  'caps_err',
-				  'all_caps')
 
 if __name__ == '__main__':
     feature_rows = []
     for line in sys.stdin:
         feature_rows.append(simplejson.loads(line.strip()))
-    
+   
+	# TODO: make typos feature run faster
+	feature_keys = [k for k in feature_rows[0].keys() if k.startswith('feature_') and k != 'feature_typos']
+
     def to_svm_input_line(feature_row):
         label = str(2*int(feature_row[LABEL_FEATURE] > 0.5) - 1)
         values = []
-        for key_idx, key in enumerate(INPUT_FEATURES):
+        for key_idx, key in enumerate(feature_keys):
             values.append("%s:%s" % (key_idx + 1, feature_row[key]))
         return ' '.join([label] + values)
     
--- review_features.py
+++ review_features.py
@@ -18,49 +18,49 @@ def __fill_special_word_freq(review, key, word_set):
     review[key] = float(word_count) / total_num_words
 
 def fill_gre_word_freq(review):
-    __fill_special_word_freq(review, 'gre_word_freq', constants.GRE_WORDS)
+    __fill_special_word_freq(review, 'feature_gre_word_freq', constants.GRE_WORDS)
 
 def fill_sat_word_freq(review):
-    __fill_special_word_freq(review, 'sat_word_freq', constants.SAT_WORDS)
+    __fill_special_word_freq(review, 'feature_sat_word_freq', constants.SAT_WORDS)
 
 def fill_illustration_word_freq(review):
-    __fill_special_word_freq(review, 'illustration_word_freq', constants.ILLUSTRATION)
+    __fill_special_word_freq(review, 'feature_illustration_word_freq', constants.ILLUSTRATION)
 
 def fill_contrast_word_freq(review):
-    __fill_special_word_freq(review, 'contrast_word_freq', constants.CONTRAST)
+    __fill_special_word_freq(review, 'feature_contrast_word_freq', constants.CONTRAST)
 
 def fill_addition_word_freq(review):
-    __fill_special_word_freq(review, 'addition_word_freq', constants.ADDITION)
+    __fill_special_word_freq(review, 'feature_addition_word_freq', constants.ADDITION)
 
 def fill_time_word_freq(review):
-    __fill_special_word_freq(review, 'time_word_freq', constants.TIME)
+    __fill_special_word_freq(review, 'feature_time_word_freq', constants.TIME)
     
 def fill_space_word_freq(review):
-    __fill_special_word_freq(review, 'space_word_freq', constants.SPACE)
+    __fill_special_word_freq(review, 'feature_space_word_freq', constants.SPACE)
 
 def fill_concession_word_freq(review):
-    __fill_special_word_freq(review, 'concession_word_freq', constants.CONCESSION)
+    __fill_special_word_freq(review, 'feature_concession_word_freq', constants.CONCESSION)
 
 def fill_comparison_word_freq(review):
-    __fill_special_word_freq(review, 'comparision_word_freq', constants.COMPARISON)
+    __fill_special_word_freq(review, 'feature_comparision_word_freq', constants.COMPARISON)
 
 def fill_emphasis_word_freq(review):
-    __fill_special_word_freq(review, 'emphasis_word_freq', constants.EMPHASIS)
+    __fill_special_word_freq(review, 'feature_emphasis_word_freq', constants.EMPHASIS)
 
 def fill_details_word_freq(review):
-    __fill_special_word_freq(review, 'details_word_freq', constants.DETAILS)
+    __fill_special_word_freq(review, 'feature_details_word_freq', constants.DETAILS)
 
 def fill_examples_word_freq(review):
-    __fill_special_word_freq(review, 'examples_word_freq', constants.EXAMPLES)
+    __fill_special_word_freq(review, 'feature_examples_word_freq', constants.EXAMPLES)
 
 def fill_consequence_word_freq(review):
-    __fill_special_word_freq(review, 'consequence_word_freq', constants.CONSEQUENCE)
+    __fill_special_word_freq(review, 'feature_consequence_word_freq', constants.CONSEQUENCE)
 
 def fill_summary_word_freq(review):
-    __fill_special_word_freq(review, 'summary_word_freq', constants.SUMMARY)
+    __fill_special_word_freq(review, 'feature_summary_word_freq', constants.SUMMARY)
 
 def fill_suggestion_word_freq(review):
-    __fill_special_word_freq(review, 'suggestion_word_freq', constants.SUGGESTION)
+    __fill_special_word_freq(review, 'feature_suggestion_word_freq', constants.SUGGESTION)
 
 def fill_review_typos(review):
     """compute the number of typos in the text of the review
@@ -70,7 +70,7 @@ def fill_review_typos(review):
     for word in words:
         if is_typo(word):
             num_typos += 1
-    review['typos'] = num_typos
+    review['feature_typos'] = num_typos
         
 def fill_review_price_range(review):
     '''assign a price range between 0 (cheap) and 3
@@ -80,15 +80,15 @@ def fill_review_price_range(review):
     amazonprice = AZ_PRODUCTS[review['product_id']]['amazonprice']
     for cat,price in enumerate(price_categories):
         if amazonprice < price:
-            review['price_range'] = cat
+            review['feature_price_range'] = cat
             break
     else:
-        review['price_range'] = 3
+        review['feature_price_range'] = 3
 
 def fill_word_count(review):
     """The number of words in the review"""
     words=re.split('\W+',review['text'])
-    review['word_count'] = len(words)-1
+    review['feature_word_count'] = len(words)-1
 
 def fill_ave_words_per_sentence(review):
     """Average number of words per sentence"""
@@ -96,12 +96,12 @@ def fill_ave_words_per_sentence(review):
     words=re.split('\W+',body)
     ends = re.compile('[.!?]+\W+')
     sentences=[m for m in ends.split(body) if len(m) > 5]
-    review['ave_words_per_sent'] = float(len(words)-1)/len(sentences)
+    review['feature_ave_words_per_sent'] = float(len(words)-1)/len(sentences)
 
 def fill_amazon_frac_voted_useful(review):
     amazon_useful = float(review.get('useful') or 0.0)
     amazon_outof = float(review.get('outof') or 0.0)
-    review['amazon_frac_voted_useful'] = amazon_useful / amazon_outof if amazon_outof else 0.0
+    review['feature_amazon_frac_voted_useful'] = amazon_useful / amazon_outof if amazon_outof else 0.0
 
 def fill_all_caps_words(review):
     """Fill ALL CAPS feature"""
@@ -111,7 +111,7 @@ def fill_all_caps_words(review):
     for word in words:
         if word.isupper() and len(word) > 1:
             num_all_caps += 1
-    review['all_caps'] = num_all_caps
+    review['feature_all_caps'] = num_all_caps
 
 def fill_capitalization_errors(review):
     """Fill Capitalization Errors"""
@@ -123,27 +123,27 @@ def fill_capitalization_errors(review):
     for sentence in sentences:
         if not sentence[0].istitle():
             num_caps_err += 1
-    review['caps_err'] = num_caps_err
+    review['feature_caps_err'] = num_caps_err
 
 def fill_num_urls(review):
     """Fill number of URLs in the review text"""
     body=review['text']
     urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', body)
-    review['num_urls'] = len(urls)
+    review['feature_num_urls'] = len(urls)
 
 def fill_valence_score(review):
     """Fill ANEW valence weighted score"""
-    review['valence_score'] = anew_scoring.weighted_freq_score(re.findall("\w+", review['text']), 
+    review['feature_valence_score'] = anew_scoring.weighted_freq_score(re.findall("\w+", review['text']), 
                                                                'valence_mean')
 
 def fill_arousal_score(review):
     """Fill ANEW arousal weighted score"""
-    review['arousal_score'] = anew_scoring.weighted_freq_score(re.findall("\w+", review['text']), 
+    review['feature_arousal_score'] = anew_scoring.weighted_freq_score(re.findall("\w+", review['text']), 
                                                                'arousal_mean')
     
 def fill_dominance_score(review):
     """Fill ANEW dominance weighted score"""
-    review['dominance_score'] = anew_scoring.weighted_freq_score(re.findall("\w+", review['text']), 
+    review['feature_dominance_score'] = anew_scoring.weighted_freq_score(re.findall("\w+", review['text']), 
                                                                  'dominance_mean')
 
 # Fill everything
