4f21be18fbc16f0f6a1a724ba6151da5b0c3fe89
Paul Wais <pwais@uscribbles.(none)>
2010-11-21 18:58:34 -0800

    make label a non-numeric

--- reviews_features.py
+++ reviews_features.py
@@ -15,9 +15,13 @@ def _fill_normalized(reviews, fill_key, feature_key):
 
 def fill_useful_percentile(reviews):
     if 'useful' in reviews[0]:
-        _fill_percentile(reviews, 'label_useful_percentile', 'useful')
+        _fill_percentile(reviews, 'useful_percentile', 'useful')
     elif 'u_count' in reviews[0]:
-        _fill_percentile(reviews, 'label_useful_percentile', 'u_count')
+        _fill_percentile(reviews, 'useful_percentile', 'u_count')
+
+def fill_useful_above_75th(reviews):
+    for review in reviews:
+        review['label_upper_quartile'] = str(review['useful_percentile'] >= 80)
 
 def fill_normalized_word_count(reviews):
     _fill_normalized(reviews, 'feature_normalized_word_count', 'word_count')
@@ -38,3 +42,4 @@ def fill_all_reviews_features(reviews):
     fill_all_mean_words_per_sentence(reviews)
     fill_all_mean_length_of_words(reviews)
     fill_normalized_url_count(reviews)
+    fill_useful_above_75th(reviews)
