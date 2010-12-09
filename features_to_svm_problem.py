a798cfe27a836268bef157e82d3a6b0de8a59143
Paul Wais <pwais@quad.local>
2010-12-08 23:18:02 -0800

    fix csv to svm problem script

--- features_to_svm_problem.py
+++ features_to_svm_problem.py
@@ -2,7 +2,7 @@
 Script to convert JSON features file to an svm_light-compatible input file.
 
 Example:
-    python features_to_svm_problem LABEL_FEATURE_NAME < features.json > svm_problem.txt
+    python features_to_svm_problem LABEL_FEATURE_NAME < features.csv > svm_problem.txt
 """
 
 import csv
@@ -10,22 +10,26 @@ import sys
 
 if __name__ == '__main__':
     label_feature_name = sys.argv[1]
-    
-    feature_rows = []
-    for line in csv.DictReader(sys.stdin):
-        feature_rows.append(line)
-   
-    feature_keys = [k for k in feature_rows[0].keys() 
-                      if (k.startswith('feature_') and
-                          # TODO: make typos feature run faster 
-                          k != 'feature_typos')]
 
+    # Start streaming input
+    feature_csv_streamer = csv.DictReader(sys.stdin)
+    
+    # Create a canonical order over feature keys
+    row = feature_csv_streamer.next()
+    feature_keys = list(row.iterkeys())
+    feature_keys.remove(label_feature_name)
+    
     def to_svm_input_line(feature_row):
+        """Defines how to write an SVM input given a feature_row dict"""
         label = str(2*int(eval(feature_row[label_feature_name])) - 1)
         values = []
         for key_idx, key in enumerate(feature_keys):
             values.append("%s:%s" % (key_idx + 1, feature_row[key]))
         return ' '.join([label] + values)
     
-    for feature_row in feature_rows:
-        print >>sys.stdout, to_svm_input_line(feature_row)
+    # Write out the first row
+    print >>sys.stdout, to_svm_input_line(row)
+    
+    # Stream out the subsequent rows
+    for row in feature_csv_streamer:
+        print >>sys.stdout, to_svm_input_line(row)
